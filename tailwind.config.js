77d55c5dd128c79415da2798c6b1dac27b2d128d
alisondraV <alice.7414122013@gmail.com>
2022-02-12 22:27:37 -0500

    Add theme colors and update headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,12 @@ module.exports = {
         'grey': '#B2B2B2',
         'light-grey': '#E2E2E2',
         'error': '#C60000',
+        'blue': '#649CD3',
+        'light-blue': '#D7EBFF',
+        'yellow': '#FFDE64',
+        'light-yellow': '#FFF4CA',
+        'green': '#A8D04B',
+        'light-green': '#ECF9CE',
       }
     },
   },
