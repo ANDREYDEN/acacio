697d1ba9368a6d764fda6e0cd6263d2a989fb05c
alisondraV <alice.7414122013@gmail.com>
2022-02-12 22:50:04 -0500

    Refactor routes list

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { Home, Setting, TickSquare, User } from 'react-iconly'
+import { Home, IconProps, Setting, TickSquare, User } from 'react-iconly'
+
+const AppRoutes: Record<string, { route: string, icon: ReactElement<IconProps> }> = {
+    Sales: {
+        route: '/sales',
+        icon: <Home />
+    },
+    People: {
+        route: '/employees',
+        icon: <User />,
+    },
+    Inventory: {
+        route: '/inventory',
+        icon: <TickSquare />,
+    },
+    Settings: {
+        route: '/settings',
+        icon: <Setting />,
+    },
+}
 
 const Menu: React.FC = ({ children }) => {
     const router = useRouter()
@@ -18,22 +37,15 @@ const Menu: React.FC = ({ children }) => {
                   <Image src='/img/acacio.svg' alt='Logo' width={156} height={31} />
               </div>
               <ul className='space-y-8 text-primary-blue font-header font-extrabold'>
-                  <li className='flex'>
-                      <Home />
-                      <h5 className='pl-6'><Link href={'/sales'}>Sales</Link></h5>
-                  </li>
-                  <li className='flex'>
-                      <User />
-                      <h5 className='pl-6'><Link href={'/employees'}>People</Link></h5>
-                  </li>
-                  <li className='flex'>
-                      <TickSquare />
-                      <h5 className='pl-6'><Link href={'/inventory'}>Inventory</Link></h5>
-                  </li>
-                  <li className='flex'>
-                      <Setting />
-                      <h5 className='pl-6'><Link href={'/settings'}>Settings</Link></h5>
-                  </li>
+                  {Object.keys(AppRoutes).map((pageName, index) => {
+                      const currentItem = AppRoutes[pageName]
+                      return (
+                          <li key={index} className='flex'>
+                              {currentItem.icon}
+                              <h5 className='pl-6'><Link href={currentItem.route}>{pageName}</Link></h5>
+                          </li>
+                      )
+                  })}
               </ul>
           </nav>
           { children }
