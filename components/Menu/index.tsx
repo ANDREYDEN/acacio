5f7bb278ce87c3d8a60128d93e935f2d3251f682
alisondraV <alice.7414122013@gmail.com>
2022-02-20 18:56:08 -0500

    Add log out button on all the pages

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { Home, Setting, TickSquare, User } from 'react-iconly'
+import { Home, Logout, Setting, TickSquare, User } from 'react-iconly'
 import { AppRoutesType } from '@interfaces'
 import MenuItem from './MenuItem'
+import { supabase } from '@client'
 
 const AppRoutes: AppRoutesType = {
     Sales: {
@@ -36,30 +37,41 @@ const AppRoutes: AppRoutesType = {
 const Menu: React.FC = ({ children }) => {
     const router = useRouter()
     const currentRoute = router.route
+    
+    const handleLogOut = async () => {
+        await supabase.auth.signOut()
+        router.replace('/')
+    }
 
     if (currentRoute === '/' || currentRoute === '/send-password-reset') {
         return (<>{ children }</>)
     }
 
     return (
-        <div className='flex'>
-            <nav className='grid place-items-center h-screen w-64 bg-light-green mr-12'>
-                <div className='absolute top-16'>
-                    <Image src='/img/acacio.svg' alt='Logo' width={156} height={31} />
-                </div>
-                <ul className='text-primary-blue font-header font-extrabold'>
-                    {Object.keys(AppRoutes).map((pageName, index) => {
-                        const currentItem = AppRoutes[pageName]
-                        return (
-                            <div key={index} className='mb-8'>
-                                <MenuItem currentItem={currentItem} currentRoute={currentRoute} pageName={pageName} />
-                            </div>
-                        )
-                    })}
-                </ul>
-            </nav>
-            { children }
-        </div>
+        <>
+            <div className='flex'>
+                <nav className='grid place-items-center h-screen w-64 bg-light-green lg:mr-12 mr-8'>
+                    <div className='absolute top-16'>
+                        <Image src='/img/acacio.svg' alt='Logo' width={156} height={31} />
+                    </div>
+                    <ul className='text-primary-blue font-header font-extrabold'>
+                        {Object.keys(AppRoutes).map((pageName, index) => {
+                            const currentItem = AppRoutes[pageName]
+                            return (
+                                <div key={index} className='mb-8'>
+                                    <MenuItem currentItem={currentItem} currentRoute={currentRoute} pageName={pageName} />
+                                </div>
+                            )
+                        })}
+                    </ul>
+                </nav>
+                { children }
+            </div>
+            <button className='absolute top-16 right-20 flex' onClick={handleLogOut}>
+                <Logout />
+                <h5 className='ml-4'>Log Out</h5>
+            </button>
+        </>
     )
 }
 
