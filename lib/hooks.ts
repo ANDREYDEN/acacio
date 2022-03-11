5d85475466f1b90a9222ecc5b1754f6edc92d689
ANDREYDEN <andrey2850@gmail.com>
2022-03-10 23:15:49 -0500

    Simplify hook

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,24 +1,17 @@
 import { supabase } from '@client'
-import { AuthChangeEvent, Session } from '@supabase/supabase-js'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 export const useUpdateAuthCookie = () => {
     useEffect(() => {
         const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-            updateSupabaseCookie(event, session)
+            axios.post('/api/auth', { event, session })
         })
     
         return () => {
             authListener?.unsubscribe()
         }
     })
-    
-    async function updateSupabaseCookie(event: AuthChangeEvent, session: Session | null) {
-        console.log('Updating Cookie with ...', { event })
-        
-        await axios.post('/api/auth', { event, session })
-    }
 }
 
 export const useMounted = () => {
