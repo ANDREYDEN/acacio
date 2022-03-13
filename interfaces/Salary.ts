5dc46886ae0842cd43510a92a321dd1389f4d54c
ANDREYDEN <andrey2850@gmail.com>
2022-03-12 23:48:09 -0500

    Clean up

--- interfaces/Salary.ts
+++ interfaces/Salary.ts
@@ -1,10 +1,2 @@
-export interface MonthlyWorkHoursDto {
-  employee_id: number
-  total_work_hours: number
-}
-
-// employee_id -> list of workhours per day
-export type MonthlyWorkHoursDto2 = Record<number, number[]>
-
 // employee_id -> statistic
 export type EmployeesMonthlyStatDto = Record<number, number>
