016960ac659a0a0f85d6751218e52a641b98d632
ANDREYDEN <andrey2850@gmail.com>
2022-03-12 22:06:18 -0500

    Add bonuses display

--- pages/employees/salary.tsx
+++ pages/employees/salary.tsx
@@ -2,12 +2,17 @@ import ErrorMessage from '@components/ErrorMessage'
 import Loader from '@components/Loader'
 import { useMounted } from '@lib/hooks'
 import { usePosterGetDeductionsForEmployees, usePosterGetSalesIncomeForEmployees } from '@lib/services/poster'
-import { useSupabaseGetEmployees } from '@lib/services/supabase'
-import { useGetEmployeesMonthlyWorkHours } from '@lib/services/supabase/salaryService'
+import { useGetEmployeesMonthlyWorkHours, useSupabaseGetBonuses, useSupabaseGetEmployees } from '@lib/services/supabase'
 import { NextPage } from 'next'
 
 const Salary: NextPage = () => {
     const { mounted } = useMounted()
+
+    const {
+        data: bonuses, 
+        loading: bonusesLoading, 
+        error: bonusesError,
+    } = useSupabaseGetBonuses()
 
     const {
         data: employees, 
@@ -34,10 +39,20 @@ const Salary: NextPage = () => {
     } = usePosterGetSalesIncomeForEmployees(employees, []) // TODO: get shifts hours
     
     
-    const loading = employeesLoading || workHoursTotalsLoading || deductionsTotalsLoading || salesIncomeTotalsLoading
+    const loading = 
+        employeesLoading || 
+        bonusesLoading || 
+        workHoursTotalsLoading || 
+        deductionsTotalsLoading || 
+        salesIncomeTotalsLoading
     if (!mounted || loading) return <Loader />
     
-    const error = employeesError || workHoursTotalsError || deductionsTotalsError || salesIncomeTotalsError
+    const error = 
+        employeesError ||
+        bonusesError || 
+        workHoursTotalsError || 
+        deductionsTotalsError || 
+        salesIncomeTotalsError
     if (error) return <ErrorMessage message={error} />
 
     return <>
@@ -47,13 +62,18 @@ const Salary: NextPage = () => {
             const salaryTotal = workHoursTotal * employee.salary
             const deductionsTotal = deductionsTotals[employee.id] ?? 0
             const salesIncomeTotal = salesIncomeTotals[employee.id] ?? 0
-            const incomeTotal = salaryTotal + salesIncomeTotal - deductionsTotal
+            const bonusesTotal = bonuses.reduce(
+                (acc, bonus) => acc + (bonus.employee_id === employee.id ? bonus.amount : 0), 
+                0
+            )
+            const incomeTotal = salaryTotal + salesIncomeTotal + bonusesTotal - deductionsTotal
             return <>
                 <h4>Name: {employee.first_name}</h4>
                 <div>Salary: {employee.salary}UAH</div>
                 <div>Hours Worked: {workHoursTotal}h</div>
                 <div>Total Salary: {salaryTotal}UAH</div>
                 <div>Deductions: {deductionsTotal}</div>
+                <div>Bonus: {bonusesTotal}</div>
                 <div>Sales Income: {salesIncomeTotal}</div>
                 <div>Total Income: {incomeTotal}</div>
             </>
