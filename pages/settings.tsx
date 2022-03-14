0b775b15896365a82a83e932f262988af0f2bd2b
alisondraV <alice.7414122013@gmail.com>
2022-03-14 19:50:37 -0400

    Add password reset form

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -6,14 +6,34 @@ import { NextPage } from 'next'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
+import Button from '@components/Button'
+import TextInput from '@components/TextInput'
+import { useForm } from 'react-hook-form'
+import ErrorMessage from '@components/ErrorMessage'
 
-export const getServerSideProps = enforceAuthenticated()
+export const getServerSideProps = enforceAuthenticated(async (context: any) => ({
+    props: {
+        ...await serverSideTranslations(context.locale, ['settings', 'common']),
+    },
+}))
 
 const Settings: NextPage = () => {
     const { mounted } = useMounted()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
     const router = useRouter()
-    const user = supabase.auth.user()
+    const { t } = useTranslation('settings')
+
+    const defaultValues = {
+        newPassword: '',
+        confirmPassword: ''
+    }
+    const { register, handleSubmit, trigger, control } = useForm({ defaultValues })
+    register('newPassword', { required: t('password.new_required').toString() })
+    register('confirmPassword', { required: t('password.confirm_required').toString() })
 
     if (!mounted) {
         return <Loader />
@@ -26,11 +46,28 @@ const Settings: NextPage = () => {
         return <Image src='/img/radiobox_unchecked.svg' width={18} height={18} alt='LanguageUnChecked' />
     }
 
+    const handleForm = async (data: any) => {
+        setError('')
+        try {
+            setLoading(true)
+            if (data.newPassword !== data.confirmPassword) {
+                throw new Error(t('password.match_error').toString())
+            }
+
+            const { error } = await supabase.auth.update({ password: data.newPassword })
+            if (error) throw new Error(error.message)
+        } catch (error: any) {
+            setError(error.error_description || error.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
         <div className='flex flex-col py-2 lg:mr-20 mr-10'>
-            <h3 className='mb-8'>Settings</h3>
-            <div className='flex rounded-xl border border-table-grey w-full p-10'>
-                <h6 className='mr-12'>Language</h6>
+            <h3 className='mb-8'>{t('header').toString()}</h3>
+            <div className='flex rounded-xl border border-table-grey w-full p-10 mb-8'>
+                <h6 className='mr-12'>{t('language').toString()}</h6>
                 <div className='mr-8 flex justify-center'>
                     {getRadioIcon('en-CA')}
                     <span className='ml-2'><Link href={router.asPath} locale='en-CA'>ENG</Link></span>
@@ -39,6 +76,33 @@ const Settings: NextPage = () => {
                     {getRadioIcon('ru-UA')}
                     <span className='ml-2'><Link href={router.asPath} locale='ru-UA'>RUS</Link></span>
                 </div>
+            </div>
+            <div className='flex flex-col rounded-xl border border-table-grey w-full p-10'>
+                <h6 className='mr-12 mb-8'>{t('password.header').toString()}</h6>
+                {loading && <Loader />}
+
+                <form className='w-96' onSubmit={handleSubmit(handleForm)}>
+                    {error && <ErrorMessage message={error} errorMessageClass='mb-8 w-full' />}
+                    <TextInput
+                        type='password'
+                        name='newPassword'
+                        label={t('password.new')}
+                        placeholder={t('password.new_placeholder')}
+                        textInputClass='mb-8'
+                        control={control}
+                        trigger={trigger}
+                    />
+                    <TextInput
+                        type='password'
+                        name='confirmPassword'
+                        label={t('password.confirm')}
+                        placeholder={t('password.confirm')}
+                        textInputClass='mb-8'
+                        control={control}
+                        trigger={trigger}
+                    />
+                    <Button label={t('save', { ns: 'common' })} buttonClass='w-full' />
+                </form>
             </div>
         </div>
     )
