05d17f5d68b72f1dbbaf5da587e1f3ee850afb4e
Andrii Denysenko <andrey2850@gmail.com>
2022-03-13 21:51:15 -0400

    Salary Table Styling (#60)
    * Put data in a table
    
    * Add header
    
    * Make bonus editable
    
    * Minor styling fixes and todo
    
    Co-authored-by: alisondraV <alice.7414122013@gmail.com>

--- components/employees/salary/SalaryTable.tsx
+++ components/employees/salary/SalaryTable.tsx
@@ -1,85 +1,64 @@
-import React, { useMemo } from 'react'
+import Table from '@components/Table'
+import NumberInputCell from '@components/NumberInputCell'
+import { SalaryTableRow } from '@interfaces'
 import { useTranslation } from 'next-i18next'
+import React, { useMemo } from 'react'
 import { Column } from 'react-table'
-import { BonusInputCell, CurrencyCell, NumberInputCell, Table } from '@components'
-import { IBonusInput, IPrepaidExpense, IRetention, SalaryTableRow } from '@interfaces'
 
 interface ISalaryTable {
-    data: SalaryTableRow[],
-    toggleModalForBonus: (bonus: IBonusInput) => void
+  data: SalaryTableRow[]
 }
 
-const SalaryTable: React.FC<ISalaryTable> = ({ data, toggleModalForBonus }: ISalaryTable) => {
-    const { t } = useTranslation('salary')
+const SalaryTable: React.FC<ISalaryTable> = ({ data }: ISalaryTable) => {
+    // TODO: internationalize this page
+    const { t } = useTranslation('schedule')
 
     const columns: Column<SalaryTableRow>[] = useMemo(
         () => [
             { 
-                Header: <h1>{t('table.employee')}</h1>,
+                Header: <h6>Employee</h6>,
                 accessor: 'employeeName'
             },
             { 
-                Header: <h1>{t('table.hourlyWage')}</h1>,
+                Header: <h6>Hourly Wage</h6>,
                 accessor: 'hourlySalary',
+                Cell: ({ value }) => <>{value} UAH/hr</>
             },
             { 
-                Header: <h1>{t('table.hoursTotal')}</h1>,
+                Header: <h6>Total Hours</h6>,
                 accessor: 'hoursTotal',
+                Cell: ({ value }) => <>{value} hrs</>
             },
             { 
-                Header: <h1>{t('table.salaryTotal')}</h1>,
+                Header: <h6>Total Salary</h6>,
                 accessor: 'salaryTotal',
-                Cell: CurrencyCell
+                Cell: ({ value }) => <>{value} UAH</>
             },
             { 
-                Header: <h1>{t('table.salesIncomeTotal')}</h1>,
+                Header: <h6>Sales Income</h6>,
                 accessor: 'salesIncomeTotal',
-                Cell: CurrencyCell
+                Cell: ({ value }) => <>{value} UAH</>
             },
             { 
-                Header: <h1>{t('table.deductionsTotal')}</h1>,
+                Header: <h6>Deductions</h6>,
                 accessor: 'deductionsTotal',
-                Cell: CurrencyCell
+                Cell: ({ value }) => <>{value} UAH</>
             },
             { 
-                Header: <h1>{t('table.prepaidExpense')}</h1>,
-                accessor: 'prepaidExpenseDto',
-                Cell: ({ value }: { value: IPrepaidExpense }) =>
-                    <NumberInputCell
-                        value={value.value.amount ?? 0}
-                        onBlur={value.onAmountChange}
-                        widthStyle='w-16'
-                    />
-            },
-            {
-                Header: <h1>{t('table.retention')}</h1>,
-                accessor: 'retentionDto',
-                Cell: ({ value }: { value: IRetention }) =>
-                    <NumberInputCell
-                        value={value.value.amount ?? 0}
-                        onBlur={value.onAmountChange}
-                        widthStyle='w-16'
-                    />
-            },
-            {
-                Header: <h1>{t('table.bonus')}</h1>,
+                Header: <h6>Bonus</h6>,
                 accessor: 'bonusDto',
-                Cell: ({ value: bonusDto }) =>
-                    <BonusInputCell
-                        bonus={bonusDto}
-                        toggleModalForBonus={() => toggleModalForBonus(bonusDto)}
-                    />
+                Cell: ({ value: bonusDto }) => <NumberInputCell value={bonusDto.initialValue} onBlur={bonusDto.onChange} widthStyle='w-20'/>
             },
             { 
-                Header: <h1>{t('table.incomeTotal')}</h1>,
+                Header: <h6>Total Income</h6>,
                 accessor: 'incomeTotal',
-                Cell: CurrencyCell
+                Cell: ({ value }) => <>{value} UAH</>
             },
         ],
-        [t, toggleModalForBonus]
+        []
     )
 
-    return <Table columns={columns} data={data} tableSpacing='px-2' />
+    return <Table columns={columns} data={data} tableSpacing='px-1' />
 }
 
 export default SalaryTable
