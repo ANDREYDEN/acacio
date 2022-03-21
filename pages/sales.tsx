d538ef3e2fe6bad40f2c1c7a53f3d4400b11592b
ANDREYDEN <andrey2850@gmail.com>
2022-03-21 17:09:06 -0400

    WIP: add dropdown

--- pages/sales.tsx
+++ pages/sales.tsx
@@ -12,6 +12,9 @@ import { useMounted } from '@lib/hooks'
 import dayjs from 'dayjs'
 import ErrorMessage from '@components/ErrorMessage'
 import useSWR from 'swr'
+import Button from '@components/Button'
+import ValidatedDropdown from '@components/ValidatedDropdown'
+import { useForm } from 'react-hook-form'
 
 export const getServerSideProps = enforceAuthenticated(async (context: any) => ({
     props: {
@@ -21,7 +24,7 @@ export const getServerSideProps = enforceAuthenticated(async (context: any) => (
 
 const Sales: NextPage = () => {
     const { mounted } = useMounted()
-    const [dateFrom, setDateFrom] = useState(dayjs().subtract(7, 'day'))
+    const [dateFrom, setDateFrom] = useState(dayjs().subtract(3, 'day'))
     const [dateTo, setDateTo] = useState(dayjs())
     const { t } = useTranslation('sales')
 
@@ -48,13 +51,44 @@ const Sales: NextPage = () => {
     [sales]
     )
 
+    const handleExport = () => {
+        // TODO: implement export
+    }
+
+    const { control } = useForm()
+
     if (!mounted || loading) {
         return <Loader />
     }
 
     return (
         <div className='flex flex-col'>
-            <h3 className='mb-8'>{t('header').toString()}</h3>
+            <div className='w-full flex justify-between mb-8'>
+                <div>
+                    <h3>{t('header').toString()}</h3>
+                    {dayjs().format('MMMM, YYYY')}
+                </div>
+                <div className='space-x-8'>
+                    <Button 
+                        label={t('export', { ns: 'common' })} 
+                        variant='secondary' 
+                        buttonClass='w-56'
+                        onClick={handleExport}
+                    />
+                </div>
+            </div>
+            <div className='w-full flex justify-between mb-8'>
+                <div className='space-x-8'>
+                    <ValidatedDropdown
+                        label={t('display', { ns: 'common' })}
+                        name='role_id'
+                        data={Object.keys(tableData).map(key => ({ label: key, value: key }))}
+                        defaultOption={t('modal.role_placeholder')}
+                        dropdownClass='mb-6' 
+                        control={control} 
+                    />
+                </div>
+            </div>
             {error && (<ErrorMessage message={`Error fetching sales: ${error}`} errorMessageClass='mb-8 w-full' />)}
             <SalesTable data={tableData} />
         </div>
