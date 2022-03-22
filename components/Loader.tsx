23f295ebd581dcb459cf2299e78c4cf5e7e98212
alisondraV <alice.7414122013@gmail.com>
2022-03-22 01:00:33 -0400

    Add a finishing touch to the spinner

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,10 +4,11 @@ const Loader: React.FC = () => {
     return (
         <div id='loader' className='animate-spin flex justify-center items-center'>
             <div
-                className='flex justify-center items-center rounded-full h-40 w-40
-                    bg-gradient-to-r from-blue to-white'
+                className='relative z-0 flex justify-center items-center h-44 w-44 rounded-full'
+                style={{ background: 'conic-gradient(#649CD3 0deg, #649CD3 30deg, white 350deg)' }}
             >
-                <div className='rounded-full h-24 w-24 bg-white' />
+                <div className='rounded-full h-28 w-28 bg-white' />
+                <div className='absolute top-0 rounded-full h-8 w-8 bg-blue'/>
             </div>
         </div>
     )
