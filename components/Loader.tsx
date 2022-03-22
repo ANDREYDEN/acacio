9ca6df4f7810e44bfc971edce155ceaf20e3fffa
alisondraV <alice.7414122013@gmail.com>
2022-03-22 00:22:35 -0400

    Update spinner styling

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Loader: React.FC = () => {
     return (
-        <div id='loader' className='flex justify-center items-center'>
-            <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500 mt-3' />
+        <div id='loader' className='animate-spin flex justify-center items-center'>
+            <div
+                className='flex justify-center items-center rounded-full h-40 w-40
+                    bg-gradient-to-r from-blue to-white'
+            >
+                <div className='rounded-full h-24 w-24 bg-white' />
+            </div>
         </div>
     )
 }
