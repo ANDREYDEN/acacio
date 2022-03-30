5faa7de6baab71c115c1131b21dc62ebf78f54a4
ANDREYDEN <andrey2850@gmail.com>
2022-03-30 17:22:53 -0400

    Address PR comments

--- components/StockTable.tsx
+++ components/StockTable.tsx
@@ -1,12 +1,9 @@
-import { Table } from '@components'
+import { CurrencyCell, NumberCell, NumberInputCell, Table } from '@components'
 import { StockTableRow } from '@interfaces'
 import { roundValue } from '@lib/utils'
 import { useTranslation } from 'next-i18next'
 import React, { useMemo } from 'react'
 import { Column } from 'react-table'
-import CurrencyCell from './CurrencyCell'
-import NumberCell from './NumberCell'
-import NumberInputCell from './NumberInputCell'
 
 interface IStockTable {
     data: StockTableRow[]
@@ -15,25 +12,6 @@ interface IStockTable {
 
 const StockTable: React.FC<IStockTable> = ({ data, selectedColumns }: IStockTable) => {
     const { t } = useTranslation('stock')
-
-    const readonlyAccessors: (keyof StockTableRow)[] = useMemo(() => [
-        'ingredientName',
-        'category',
-        'supplier',
-        'initialBalance',
-        'initialAvgCost',
-        'sold',
-        'soldCost',
-        'writeOff',
-        'writeOffCost',
-        'lastSupply',
-        'finalBalance',
-        'finalBalanceCost',
-        'finalAverageCost',
-        'reorder',
-        'toOrder',
-        'totalCost',
-    ], [])
 
     const columns: Column<StockTableRow>[] = useMemo<Column<StockTableRow>[]>(
         () => [
