431a17db0cde1876adbfffe9de280a186c4c90d6
Alisa Vynohradova <56138100+alisondraV@users.noreply.github.com>
2022-04-02 20:37:33 -0400

    Bonuses comments (#107)
    * Create a BonusInputCell component
    
    * Create a modal for updating bonus comment
    
    * Make modal work for updating the comments
    
    * Indicate whether there is a comment for a bonus
    
    * Address PR comment

--- components/employees/salary/BonusInputCell.tsx
+++ components/employees/salary/BonusInputCell.tsx
@@ -18,7 +18,6 @@ const BonusInputCell: React.FC<IBonusInputCell> = ({ bonus, toggleModalForBonus 
                 value={cellValue}
                 onChange={(e) => setCellValue(+e.target.value)}
                 onBlur={() => bonus.onAmountChange(cellValue)}
-                onFocus={(e) => e.target.select()}
             />
             <button
                 className={`flex-1 flex justify-center items-center h-full border-l border-grey
--- components/employees/salary/index.ts
+++ components/employees/salary/index.ts
@@ -1,1 +1,3 @@
 export { default as SalaryTable } from './SalaryTable'
+export { default as BonusInputCell } from './BonusInputCell'
+export { default as BonusCommentModal } from './BonusCommentModal'
